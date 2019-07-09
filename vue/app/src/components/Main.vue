3068678960f4b9f0bcd040dda8441604c7264689
dd0125 <dd.0125.jp@gmail.com>
2019-07-10 00:11:55 +0900

    first commit

---
+++ vue/app/src/components/Main.vue
@@ -0,0 +1,24 @@
+<script>
+import Sub from '@/components/Sub'
+export default {
+  components: { Sub }
+}
+</script>
+
+<template>
+    <div>
+        Main
+        <div class="sub-area">
+            <Sub color="#f00"/>
+            <Sub color="#0f0"/>
+            <Sub color="#00f"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.sub-area {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ vue/app/src/components/Sub.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :style="textStyle">
+        Sub
+        {{ color }}
+    </div>
+</template>
+<style scoped>
+div {
+  font-weight: bold;
+  margin: 20px;
+}
+</style>
+
+<script>
+export default {
+  props: ['color'],
+  computed: {
+    textStyle () {
+      return 'color: ' + this.color
+    }
+  }
+}
+</script>
